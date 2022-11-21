e638900b014ca261edf9fec8e7248ce1aa09617a
Joshua Greatorex <64132209+Josh-greatorex@users.noreply.github.com>
2022-11-21 08:36:18 +0000

    Add files via upload

---
+++ pTable2/pTable2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Please enter either a symbol or a number of the element on the periodic table");
+string userInput;
+userInput = Console.ReadLine();
+DataTable elementsTable = new DataTable("elements");
+try
+{
+    using (SqlConnection _con = new SqlConnection("Server=localhost;Database=pTable;Integrated Security=True"))
+    {
+        int atomicNumber;
+        string queryStatement;
+        if (int.TryParse(userInput, out atomicNumber))
+        {
+            queryStatement = $"SELECT * FROM [pTable].[dbo].[elements] WHERE [atomic_number] = {userInput}";
+        }
+        else
+        {
+            queryStatement = $"SELECT * FROM [pTable].[dbo].[elements] WHERE [symbol] = \'{userInput}\'";
+        }
+         
+        using (SqlCommand _cmd = new SqlCommand(queryStatement, _con))
+        {
+            
+            SqlDataAdapter _dap = new SqlDataAdapter(_cmd);
+
+            _con.Open();
+            _dap.Fill(elementsTable);
+            _con.Close();
+        }
+    }
+}
+catch (SqlException e)
+{
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine(elementsTable.Rows.Count);
+foreach (DataRow dataRow in elementsTable.Rows)
+{
+    foreach (var item in dataRow.ItemArray)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Console.ReadLine();
---
+++ pTable2/sql/insert test row.sql
@@ -0,0 +1,3 @@
+INSERT INTO elements ([name], symbol, atomic_number, atomic_mass, [group], [period]) VALUES
+
+('Hydrogen',  'H','1',  '1.00794','1','1');
